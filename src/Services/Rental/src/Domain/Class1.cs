f3fe3e248b9438e5328b29e6cf9410d3a2bd429a
erikyudi <erik.kashihara@gmail.com>
2025-03-12 17:33:43 -0300

    🚀 Initial project setup with Clean Architecture structure

---
+++ src/Services/Rental/src/Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Rental.Domain;
+
+public class Class1
+{
+
+}
---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Rental.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
